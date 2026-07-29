e4634233b666fa0be18a34cb38a2255b8796feb6
Tamiliniyan Chandrasekar <tamiliniyanchandru@gmail.com>
2026-07-29 10:10:57 +0530

    Enable Nitro plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
-import react from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [
-  tailwindcss(),
-  tanstackStart({
-    server: { entry: "server" },
-  }),
-  react(),
-],
+    tailwindcss(),
+    tanstackStart({
+      server: {
+        entry: "server",
+      },
+    }),
+    nitro(),
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
